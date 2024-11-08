1da75adb40ec998a86c4e3f89363c03274155c00
ViktoriaShcherbakova <Viktoriia.Shcherbakova.V@gmail.com>
2024-11-08 18:25:38 +0200

    hw3 updated

--- hw3.js
+++ hw3.js
@@ -1,4 +1,4 @@
-function checkProbabilityTheory(min, max, count) {
+function checkProbabilityTheory(count, min = 100, max = 1000) {
     let arrayOfNumbers = [];
     for (let i = 0; i < count; i++) {
         let random = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -23,5 +23,5 @@ function checkProbabilityTheory(min, max, count) {
     }
 }
 
-let test = checkProbabilityTheory(100, 1000, 10000);
+let test = checkProbabilityTheory(10000);
 console.log(test);
