bf10ed0c2addfbe43ab03fc11cd4189bc2f3bf01
ViktoriaShcherbakova <Viktoriia.Shcherbakova.V@gmail.com>
2024-11-13 17:02:57 +0200

    hw4 updated

--- hw4.2.js
+++ hw4.2.js
@@ -1,35 +1,39 @@
 let services = {
     "стрижка": "60.01 грн",
     "гоління": "80.90 грн",
-    "Миття голови": "100 грн"
+    "Миття голови": "100 грн",
+    price: function () {
+        let sum = 0;
+        for (let key in this) {
+            if (typeof this[key] !== 'function') {
+                sum += parseFloat(this[key]);
+            }
+        }
+        console.log(sum);
+    },
+    minPrice: function () {
+        let prices = [];
+        for (let key in this) {
+            if (typeof this[key] !== 'function') {
+                prices.push(parseFloat(this[key]));
+            }
+        }
+        console.log(Math.min(... prices));
+    },
+    maxPrice: function () {
+        let prices = [];
+        for (let key in this) {
+            if (typeof this[key] !== 'function') {
+                prices.push(parseFloat(this[key]));
+            }
+        }
+        console.log(Math.max(... prices));
+    }
 };
 
+services.price();
 services['Розбити скло'] = "200.99 грн";
 
-function price() {
-    let sum = 0;
-    for (let key in this) {
-        sum += parseFloat(this[key]);
-    }
-    return sum;
-}
-console.log(price.call(services));
-
-function minPrice() {
-    let prices = [];
-    for (let key in this) {
-        prices.push(parseFloat(this[key]));
-    }
-    return Math.min(... prices);
-}
-console.log(minPrice.call(services));
-
-function maxPrice() {
-    let prices = [];
-    for (let key in this) {
-        prices.push(parseFloat(this[key]));
-    }
-    return Math.max(... prices);
-}
-console.log(maxPrice.call(services));
-
+services.price();
+services.minPrice();
+services.maxPrice();
